5861fa45c0917edcc229d5e4facf6be3bdd95f57
Lee <123798682+Calmcat1@users.noreply.github.com>
2023-05-04 13:01:50 +0300

    Add files via upload

---
+++ JS/RMSystem.js
@@ -0,0 +1,50 @@
+function pageRotator(Page,elmnt){
+
+  let tabcontent, tablinks, i;
+
+  tabcontent = document.getElementsByClassName("content");
+
+  for(i=0; i<tabcontent.length; i++){
+    tabcontent[i].style.display = "none";
+  }
+
+  //set tablinks
+  document.getElementById(Page).style.display = "block";
+
+  //clear whitebtn
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for(i=0; i<tablinks.length; i++){
+    tablinks[i].style.opacity = "1";
+  }
+
+  elmnt.style.opacity = "0.4"
+}
+
+
+
+function retractor(){
+
+  let tablinks;
+
+  document.getElementById("nav-igation").style.width = 0;
+
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for(i=0; i<tablinks.length; i++){
+    tablinks[i].style.opacity = 0;
+
+  }
+}
+
+function protractor(){
+  document.getElementById("nav-igation").style.width = "300px";
+
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for(i=0; i<tablinks.length; i++){
+    tablinks[i].style.opacity = 1;
+    
+  }
+}
+
